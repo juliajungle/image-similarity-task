57385d4287b530f436990065785fe2bfe405ec1c
juliajungle <julesrlax@gmail.com>
2021-04-03 20:57:43 +0100

    app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import styled from "styled-components";
 
-function App() {
+import "./App.css";
+import originalImageData from "./data.json";
+import { search } from "./utils/search";
+import images from "./images";
+import { useEffect, useState } from "react";
+import { ImageGallery } from "./components/ImageGallery";
+import { SimilarImages } from "./components/SimilarImages";
+import { normaliseData } from "./utils/normaliseData";
+
+const StyledImage = styled.img`
+  display: inherit;
+  width: 400px;
+  margin: 10px auto;
+`;
+
+const StyledWrapperDiv = styled.div`
+  width: 100%;
+  margin: auto;
+`;
+
+const StyledButton = styled.button`
+  padding: 10px;
+  background-color: black;
+  color: white;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  :hover {
+    transform: scale(1.1);
+  }
+`;
+
+const App = () => {
+  const [similarImages, setSimilarImages] = useState([]);
+  const [imageData, setImageData] = useState([]);
+  const [compareImage, setCompareImage] = useState();
+  const [showGallery, setShowGallery] = useState(true);
+
+  useEffect(() => {
+    const normalisedData = normaliseData(originalImageData);
+    setImageData(normalisedData);
+  }, []);
+
+  const setChosenImage = (imageId) => {
+    const imageNumber = imageId.replace(".jpg", "");
+    setCompareImage(imageNumber);
+    setShowGallery(false);
+    setSimilarImages(search(imageId, imageData));
+  };
+
+  const showGalleryView = () => {
+    setShowGallery(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Similarity Task</h1>
+
+      {showGallery && (
+        <Fragment>
+          {" "}
+          <p>Choose an image to find other similar ones</p>{" "}
+          <ImageGallery images={imageData} setChosenImage={setChosenImage} />
+        </Fragment>
+      )}
+      {!showGallery && (
+        <Fragment>
+          <StyledWrapperDiv>
+            <StyledImage
+              src={images[compareImage - 1]}
+              alt={`image${compareImage}`}
+              id={`image${compareImage}`}
+            />
+          </StyledWrapperDiv>
+          <StyledButton onClick={showGalleryView}>Back to gallery</StyledButton>
+        </Fragment>
+      )}
+
+      <p>Other images like yours</p>
+      <SimilarImages images={similarImages} />
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders intro text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const pElement = screen.getByText(/Choose an image/i);
+  expect(pElement).toBeInTheDocument();
 });
